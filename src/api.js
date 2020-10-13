854c08ff896d06047fada073d856a7f311ed726e
Maxim <yourstenex@gmail.com>
2020-10-13 21:24:39 +0300

    changed api http to https

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const { default: Axios } = require('axios');
 
 const instance = Axios.create({
-  baseURL: 'http://api.openweathermap.org/data/2.5/',
+  baseURL: 'https://api.openweathermap.org/data/2.5/',
 });
 
 export default instance;
